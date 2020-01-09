bd92906dabdbf763a862cfbaa3a196b0b5dd9be4
林圣东 <497291309@qq.com>
2020-01-09 17:47:34 +0800

    hh

--- music/src/components/pages/player.vue
+++ music/src/components/pages/player.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="player">
-    <div class="bg"><img src="../../../static/images/timg1.jpg" alt=""></div>
+    <div class="bg"><img src="../../../static/images/20150718092902357590.jpg" alt=""></div>
     <div class="wrap">
-      <div class="poster"><img src="../../../static/images/ldh.jpg" alt=""></div>
-      <div class="info">
-        <div class="music-title">一起走过的日子</div>
-        <div class="singer">刘德华</div>
-        <div class="lyric">
-          <p v-for="(e, index) in lyric" :key="index">{{e.LrcLine}}</p>
+      <div class="content">
+        <div class="poster"><div class="poster-wrap"><img src="../../../static/images/20150718092902357590.jpg" alt=""></div></div>
+        <div class="info">
+          <div class="music-title">届かない恋</div>
+          <div class="singer"><span>歌手：</span>上原れな</div>
+          <div class="lyric">
+            <div class="lyric-wrap">
+              <p :class="{'active-line':index==10}" v-for="(e, index) in lyric" :key="index">{{e.LrcLine}}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <audio id="music" ref="music" src="../../../static/images/届かない恋 .mp3"></audio>
+	  <div class="music-bar" id="music_bar">
+      <div class="music-bar-bg"><img src="../../../static/images/20150718092902357590.jpg" alt=""></div>
+	  	<div class="music-bar-wrap">
+        <!-- 控制菜单 -->
+	      <div id="control">
+	      	<div class="previous" id="previous"><i class="icon-previous"></i></div>
+	      	<div class="play" @click="playClick()"><i :class="playBtn"></i></div>
+	      	<div class="previous" id="next"><i class="icon-next"></i></div>
+	      </div>
+	      <!-- 进度条 -->
+	      <div class="msg" id="msg">
+	      	<div id="m_progress" ref="mProgress">
+	      		<div id="m_p_icon" ref="mProgressIcon"></div>
+	      	</div>	
+	      	<span id="m_time">{{playTime}}</span>	
+	      </div>
+	      <!-- 音量 -->
+	      <div class="voice">
+	      	<div class="v_icon"><i class="icon-voice"></i></div>
+	      	<div id="v_progress">
+	      		<div id="v_value"></div>
+	      	</div>		
+	      </div>
+      </div>
+    </div><!-- music_bar end --> 
   </div>
 </template>
 
@@ -19,233 +49,233 @@ export default {
   data(){
     return{
       lyric: [
-        {
+             {
           "type": 1,
           "Time": null,
           "TimeMs": 0,
-          "LrcLine": "菊花台"
+          "LrcLine": "届かない恋 ’13"
         },
         {
           "type": 0,
           "Time": null,
           "TimeMs": 0,
-          "LrcLine": "周杰伦"
+          "LrcLine": "上原れな"
         },
         {
           "type": 2,
           "Time": null,
           "TimeMs": 0,
-          "LrcLine": "依然范特西"
+          "LrcLine": "届かない恋 ’13"
         },
         {
           "type": 3,
           "Time": null,
           "TimeMs": 0,
-          "LrcLine": "style海峰"
+          "LrcLine": "Vine"
         },
         {
           "type": 5,
-          "Time": "00:00.40",
-          "TimeMs": 40,
-          "LrcLine": "周杰伦 - 菊花台"
+          "Time": "00:00.53",
+          "TimeMs": 53,
+          "LrcLine": "「届かない恋 ’13」"
         },
         {
           "type": 5,
-          "Time": "00:03.24",
-          "TimeMs": 3024,
-          "LrcLine": "作词：方文山 作曲：周杰伦"
+          "Time": "00:03.53",
+          "TimeMs": 3053,
+          "LrcLine": "作詞∶須谷尚子"
         },
         {
           "type": 5,
-          "Time": "00:06.29",
-          "TimeMs": 6029,
-          "LrcLine": "编曲：钟兴民"
+          "Time": "00:06.53",
+          "TimeMs": 6053,
+          "LrcLine": "作曲∶石川真也"
         },
         {
           "type": 5,
-          "Time": "00:09.79",
-          "TimeMs": 9079,
-          "LrcLine": "LRC制作： 惜  fQQ:727991729"
+          "Time": "00:09.53",
+          "TimeMs": 9053,
+          "LrcLine": "編曲∶小林俊太郎"
         },
         {
           "type": 5,
-          "Time": "00:36.01",
-          "TimeMs": 36001,
-          "LrcLine": "你的泪光 柔弱中带伤"
+          "Time": "00:12.53",
+          "TimeMs": 12053,
+          "LrcLine": "歌∶上原れな"
         },
         {
           "type": 5,
-          "Time": "00:43.00",
-          "TimeMs": 43000,
-          "LrcLine": "惨白的月弯弯 勾住过往"
+          "Time": "00:16.53",
+          "TimeMs": 16053,
+          "LrcLine": "TVアニメ「WHITE ALBUM2」OPテーマ"
         },
         {
           "type": 5,
-          "Time": "00:49.75",
-          "TimeMs": 49075,
-          "LrcLine": "夜太漫长 凝结成了霜"
+          "Time": "00:24.53",
+          "TimeMs": 24053,
+          "LrcLine": "孤独なふりをしてるの?"
         },
         {
           "type": 5,
-          "Time": "00:56.46",
-          "TimeMs": 56046,
-          "LrcLine": "是谁在阁楼上 冰冷的绝望"
+          "Time": "00:31.52",
+          "TimeMs": 31052,
+          "LrcLine": "なぜだろう 気になっていた"
         },
         {
           "type": 5,
-          "Time": "01:03.75",
-          "TimeMs": 63075,
-          "LrcLine": "雨轻轻弹 朱红色的窗"
+          "Time": "00:38.64",
+          "TimeMs": 38064,
+          "LrcLine": "気づけば いつのまにか"
         },
         {
           "type": 5,
-          "Time": "01:10.58",
-          "TimeMs": 70058,
-          "LrcLine": "我一生在纸上 被风吹乱"
+          "Time": "00:45.71",
+          "TimeMs": 45071,
+          "LrcLine": "誰より 惹かれていた"
         },
         {
           "type": 5,
-          "Time": "01:17.68",
-          "TimeMs": 77068,
-          "LrcLine": "梦在远方 化成一缕香"
+          "Time": "00:54.20",
+          "TimeMs": 54020,
+          "LrcLine": "どうすれば この心は 鏡に映るの?"
         },
         {
           "type": 5,
-          "Time": "01:24.39",
-          "TimeMs": 84039,
-          "LrcLine": "随风飘散 你的模样"
+          "Time": "01:07.98",
+          "TimeMs": 67098,
+          "LrcLine": "届かない恋をしていても"
         },
         {
           "type": 5,
-          "Time": "01:34.39",
-          "TimeMs": 94039,
-          "LrcLine": "菊花残 满地伤 你的笑容已泛黄"
+          "Time": "01:15.05",
+          "TimeMs": 75005,
+          "LrcLine": "映しだす日がくるかな"
         },
         {
           "type": 5,
-          "Time": "01:41.91",
-          "TimeMs": 101091,
-          "LrcLine": "花落人断肠 我心事 静静淌  "
+          "Time": "01:21.10",
+          "TimeMs": 81010,
+          "LrcLine": "ぼやけた答えが"
         },
         {
           "type": 5,
-          "Time": "01:48.10",
-          "TimeMs": 108010,
-          "LrcLine": "北风乱 夜未央 你的影子剪不断"
+          "Time": "01:24.68",
+          "TimeMs": 84068,
+          "LrcLine": "見え始めるまでは"
         },
         {
           "type": 5,
-          "Time": "01:55.77",
-          "TimeMs": 115077,
-          "LrcLine": "徒留我孤单在湖面 成双"
+          "Time": "01:29.89",
+          "TimeMs": 89089,
+          "LrcLine": "今もこの恋は 動き出せない"
         },
         {
           "type": 5,
-          "Time": "02:30.70",
-          "TimeMs": 150070,
-          "LrcLine": "花已向晚 飘落了灿烂"
+          "Time": "01:56.89",
+          "TimeMs": 116089,
+          "LrcLine": "初めて声をかけたら"
         },
         {
           "type": 5,
-          "Time": "02:37.67",
-          "TimeMs": 157067,
-          "LrcLine": "凋谢的世道上 命运不堪"
+          "Time": "02:03.93",
+          "TimeMs": 123093,
+          "LrcLine": "振り向いてくれたあの日"
         },
         {
           "type": 5,
-          "Time": "02:44.67",
-          "TimeMs": 164067,
-          "LrcLine": "愁莫渡江 秋心拆两半"
+          "Time": "02:11.08",
+          "TimeMs": 131008,
+          "LrcLine": "あなたは 眩しすぎて"
         },
         {
           "type": 5,
-          "Time": "02:51.40",
-          "TimeMs": 171040,
-          "LrcLine": "怕你上不了岸 一辈子摇晃"
+          "Time": "02:18.11",
+          "TimeMs": 138011,
+          "LrcLine": "まっすぐ見れなかった"
         },
         {
           "type": 5,
-          "Time": "02:58.65",
-          "TimeMs": 178065,
-          "LrcLine": "谁的江山 马蹄声狂乱"
+          "Time": "02:26.63",
+          "TimeMs": 146063,
+          "LrcLine": "どうすれば その心に 私を写すの?"
         },
         {
           "type": 5,
-          "Time": "03:05.76",
-          "TimeMs": 185076,
-          "LrcLine": "我一身的戎装 呼啸沧桑"
+          "Time": "02:40.41",
+          "TimeMs": 160041,
+          "LrcLine": "叶わない恋をしていても"
         },
         {
           "type": 5,
-          "Time": "03:12.82",
-          "TimeMs": 192082,
-          "LrcLine": "天微微亮 你轻声地叹"
+          "Time": "02:47.34",
+          "TimeMs": 167034,
+          "LrcLine": "写しだす日がくるかな"
         },
         {
           "type": 5,
-          "Time": "03:20.28",
-          "TimeMs": 200028,
-          "LrcLine": "一夜惆怅 如此委婉"
+          "Time": "02:53.51",
+          "TimeMs": 173051,
+          "LrcLine": "ぼやけた答えが"
         },
         {
           "type": 5,
-          "Time": "03:29.27",
-          "TimeMs": 209027,
-          "LrcLine": "菊花残 满地伤 你的笑容已泛黄"
+          "Time": "02:57.10",
+          "TimeMs": 177010,
+          "LrcLine": "少しでも見えたら"
         },
         {
           "type": 5,
-          "Time": "03:37.09",
-          "TimeMs": 217009,
-          "LrcLine": "花落人断肠 我心事 静静淌"
+          "Time": "03:02.35",
+          "TimeMs": 182035,
+          "LrcLine": "きっとこの恋は 動き始める"
         },
         {
           "type": 5,
-          "Time": "03:43.19",
-          "TimeMs": 223019,
-          "LrcLine": "北风乱 夜未央 你的影子剪不断"
+          "Time": "03:34.16",
+          "TimeMs": 214016,
+          "LrcLine": "どうすれば この心は 鏡に映るの?"
         },
         {
           "type": 5,
-          "Time": "03:50.91",
-          "TimeMs": 230091,
-          "LrcLine": "徒留我孤单在湖面 成双"
+          "Time": "03:47.95",
+          "TimeMs": 227095,
+          "LrcLine": "届かない恋をしていても"
         },
         {
           "type": 5,
-          "Time": "04:00.55",
-          "TimeMs": 240055,
-          "LrcLine": "菊花残 满地伤 你的笑容已泛黄"
+          "Time": "03:54.83",
+          "TimeMs": 234083,
+          "LrcLine": "映しだす日がくるかな"
         },
         {
           "type": 5,
-          "Time": "04:08.18",
-          "TimeMs": 248018,
-          "LrcLine": "花落人断肠 我心事 静静淌"
+          "Time": "04:01.08",
+          "TimeMs": 241008,
+          "LrcLine": "ぼやけた答えが"
         },
         {
           "type": 5,
-          "Time": "04:14.39",
-          "TimeMs": 254039,
-          "LrcLine": "北风乱 夜未央 你的影子剪不断"
+          "Time": "04:04.67",
+          "TimeMs": 244067,
+          "LrcLine": "見え始めるまでは"
         },
         {
           "type": 5,
-          "Time": "04:24.09",
-          "TimeMs": 264009,
-          "LrcLine": "徒留我孤单在湖面 成双"
+          "Time": "04:09.94",
+          "TimeMs": 249094,
+          "LrcLine": "今もこの恋は 動き出せない"
         },
         {
           "type": 5,
-          "Time": "04:46.69",
-          "TimeMs": 286069,
-          "LrcLine": " 惜  fQQ:727991729"
+          "Time": "04:40.65",
+          "TimeMs": 280065,
+          "LrcLine": "Lrc By VINE (C) www.22lrc.com"
         },
         {
           "type": 5,
-          "Time": "99:99.99",
-          "TimeMs": 6039099,
-          "LrcLine": "◇"
+          "Time": "04:45.65",
+          "TimeMs": 285065,
+          "LrcLine": "終わり"
         },
         {
           "type": 5,
@@ -254,11 +284,61 @@ export default {
           "LrcLine": "LRC歌词下载：www.22lrc.com "
         }
       ],
+      /* 音乐播放器控制参数 */
+      music: '',//音频文件
+      mProgress: '',//进度条
+      mProgressIcon: '',//进度条头上的那个点
+      playBtn: 'icon-play',
+      playTime: 0,//进度条右下角播放时间
+      playTimeLength: -1,//当前播放时长
+      progressWidth: 0,//当前播放的进度条长度
+      /* 音乐播放器控制参数 end */
     }
   },
   methods:{
-
+    playClick(){
+      if( this.music.paused ){
+        this.music.play();//播放音乐
+        this.playBtn = "icon-pause";
+			}else{
+        this.music.pause();
+        this.playBtn = "icon-play"
+      }
+      //启动时间监听钩子
+      this.music.ontimeupdate = function(){
+        
+      }
+    },
+    //获取音频长度
+    getDuration() {
+       console.log(this.$refs.music.duration); //此时可以获取到duration
+       //this.duration = this.$refs.audio.duration;
+    },
   },
+  mounted(){
+    //赋值
+    this.music = this.$refs.music;
+    this.mProgress = this.$refs.mProgress;
+    this.mProgressIcon = this.$refs.mProgressIcon;
+    
+    //计算
+    setTimeout(() => {
+      let audio = document.getElementById("music");
+      let mLength = audio.duration;//总时长
+      let minutes = parseInt( mLength / 60 );
+      let seconds = parseInt( mLength - minutes*60 );
+      let m = minutes<10? '0'+ minutes : minutes;
+      let s = seconds<10? '0' + seconds : seconds;
+      this.playTime = `${m}:${s}/00:00`;
+    }, 100);
+    
+    
+  },
+  watch:{
+    playTimeLength(){
+      
+    }
+  }
 }
 </script>
 
