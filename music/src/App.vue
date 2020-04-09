e3826068d7d45e7d0151c72572c4b1ce2fdced5b
林圣东 <497291309@qq.com>
2020-04-09 11:15:02 +0800

    添加看板娘

--- music/src/App.vue
+++ music/src/App.vue
@@ -5,8 +5,21 @@
 </template>
 
 <script>
+
 export default {
   name: 'App',
+  mounted() {
+    window.L2Dwidget.init({
+      "display":{
+        'superSample': 2,
+        "width": 200,
+        "height": 400,
+        "position": "left",
+        "hOffset": 0,
+        "vOffset": 0
+      }
+    })
+  }
 }
 </script>
 
